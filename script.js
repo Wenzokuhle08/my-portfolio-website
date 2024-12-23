5d4c070fd288db26a66f0f0828eeb51732f1c99f
Wenzokuhle08 <wmaziya023@student.wethinkcode.co.za>
2024-12-23 22:45:53 +0200

    add new styles, and files

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Welcome to my portfolio!");
 
-  // Dynamic text functionality
+
   const dynamicText = document.getElementById('dynamic');
   const texts = ['Wenzokuhle Maziya', 'a Full Stack Developer', 'and a UX/UI Designer'];
   let index = 0;
 
   setInterval(() => {
-    dynamicText.style.opacity = 0; // Start fade-out effect
+    dynamicText.style.opacity = 0; 
     setTimeout(() => {
-      dynamicText.textContent = texts[index]; // Change the text
-      dynamicText.style.opacity = 1; // Fade back in
-      index = (index + 1) % texts.length; // Cycle through the texts
-    }, 500); // Change the text after 500ms fade-out
-  }, 3000); // Change text every 3 seconds
+      dynamicText.textContent = texts[index]; 
+      dynamicText.style.opacity = 1; 
+      index = (index + 1) % texts.length; 
+    }, 500); 
+  }, 3000); 
    document.querySelectorAll('.skill-box').forEach(box => {
         box.addEventListener('mousedown', () => {
             box.style.transform = 'scale(0.95)';
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
 
         box.addEventListener('mouseleave', () => {
-            box.style.transform = 'scale(1)'; // Reset when the mouse leaves the box
+            box.style.transform = 'scale(1)'; 
         });
     });
 });
